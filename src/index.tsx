52d88e5dcd53292558f8eda5e2304fca8426e855
BeeMOre32 <windowssart01@gmail.com>
2022-06-24 13:26:06 +0900

    last update!

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
@@ -12,12 +12,12 @@ const root = ReactDOM.createRoot(
 const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>{" "}
       </QueryClientProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,63 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { PriceInfo } from "../interface/PriceInfo";
+import { fetchPrice } from "./fetch";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Context = styled.div`
+  width: 100%;
+  height: 40px;
+  border-radius: 10px;
+  margin-top: 30px;
+  background-color: ${(props) => props.theme.textColor};
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span {
+    background-color: ${(props) => props.theme.textColor};
+    color: ${(props) => props.theme.bgColor};
+  }
+`;
+
 function Price() {
-  return <h1>Price</h1>;
+  const { coinId } = useParams();
+  const { data, isLoading } = useQuery<PriceInfo>(["tickers", coinId], () =>
+    fetchPrice(coinId)
+  );
+
+  return (
+    <>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <Wrapper>
+          <Context>
+            <span>Current Price: {data?.quotes.USD.price}$</span>
+          </Context>
+          <Context>
+            <span>
+              1 Hours ago rate of chagne: {data?.quotes.USD.percent_change_1h}%
+            </span>
+          </Context>
+          <Context>
+            <span>
+              30 Min ago rate of chagne: {data?.quotes.USD.percent_change_30m}%
+            </span>
+          </Context>
+          <Context>
+            <span>
+              15 Min ago rate of chagne: {data?.quotes.USD.percent_change_15m}%
+            </span>
+          </Context>
+        </Wrapper>
+      )}
+    </>
+  );
 }
 
 export default Price;
